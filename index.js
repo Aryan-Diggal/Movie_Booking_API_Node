721c20130b211e68b0fd9d425ccc32cd525a69af
Aryan-Diggal <aryandiggal26@gmail.com>
2026-04-03 12:16:43 +0530

    "Feature: Added basic movie create API"

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const env = require("dotenv");
 const mongoose = require("mongoose");
-const Movie = require("./models/movie.model");
 
+const movieRoutes = require("./routes/movie.routes");
 
 env.config();
 const app = express();  
@@ -23,6 +23,8 @@ app.get("/home", (req, res) => {
     })
 })
 
+movieRoutes(app); // invoking movie routes
+
 
 
 app.listen(process.env.PORT, async () => {
@@ -33,20 +35,20 @@ app.listen(process.env.PORT, async () => {
         await mongoose.connect(process.env.DB_URL);
         console.log("Successfully connected to Mongo");
 
-        await Movie.create({
-            name: "Dhurandhar",
-            description: `An underworld saga following a network of 
-            criminals, informants and operatives whose lives intersect, 
-            navigating covert operations, espionage and betrayals.`,
-            casts: ["Ranveer Singh", "Sara Arjun", "Akshaye Khanna", 
-                "Arjun Rampal", " Rakseh Bedi", "R. Madhavan", "Sanjay Dutt"],
+        // await Movie.create({
+        //     name: "Dhurandhar",
+        //     description: `An underworld saga following a network of 
+        //     criminals, informants and operatives whose lives intersect, 
+        //     navigating covert operations, espionage and betrayals.`,
+        //     casts: ["Ranveer Singh", "Sara Arjun", "Akshaye Khanna", 
+        //         "Arjun Rampal", " Rakseh Bedi", "R. Madhavan", "Sanjay Dutt"],
             
-            trailerURL: "https://Dhurandhar/trailer/1",
-            language: "Hindi",
-            releaseDate: "12/05/2026", 
-            director: "Aditya Dhar",
-            releaseStatus: "RELEASED",
-        });
+        //     trailerURL: "https://Dhurandhar/trailer/1",
+        //     language: "Hindi",
+        //     releaseDate: "12/05/2026", 
+        //     director: "Aditya Dhar",
+        //     releaseStatus: "RELEASED",
+        // });
 
     } catch (error) {
         console.log("Not able to connect to Mongo", error);

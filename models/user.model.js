db2a2bef42588d87a232e58611e66fdbee7e99b2
Aryan-Diggal <aryandiggal26@gmail.com>
2026-04-20 00:35:29 +0530

    Feature: Encrypted plain passwords during registration

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -29,6 +30,11 @@ const userSchema = new mongoose.Schema({
         default: "APPROVED"
     }
 }, {timestamps: true});
+
+userSchema.pre("save", async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;

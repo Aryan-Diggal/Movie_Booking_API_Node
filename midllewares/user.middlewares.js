8f55b5ebedc2bb774681501208673c2f24c43d1e
Aryan-Diggal <aryandiggal26@gmail.com>
2026-04-28 03:29:42 +0530

    Feat: added validations for the incoming request in reset password and update user

--- midllewares/user.middlewares.js
+++ midllewares/user.middlewares.js
@@ -4,10 +4,12 @@ const validateUpdateUserRequest = (req, res, next) => {
 
     // Validates the presence of atleast one of the things 
 
-    if(!(req.body.oldPassword || req.body.newPassword)){
-        errorResponseBody.err = "Malformed request, please send atleast one param"
+    if(!(req.body.userRole || req.body.userStatus)){
+        errorResponseBody.err = "Malformed request, please send atleast one parameter"
         return res.status(400).json(errorResponseBody);
     }
+
+    next();
 }
 
 module.exports = {
